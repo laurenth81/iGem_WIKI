eda1ed04676f2461df5e7f8a063ae2d480ed8373
LK152 <wuluke0@gmail.com>
2021-07-27 11:08:47 +0800

    big changes

--- nav.js
+++ nav.js
@@ -3,4 +3,10 @@ trackButton.addEventListener("click", menuBarSwitch);
 function menuBarSwitch(){
     var x = document.getElementById("menuButton");
     x.classList.toggle("transit");
+    var y = document.getElementById("navBar");
+    if (y.className === "menuBar"){
+        y.className += " dropDown";
+    }else{
+        y.className = "menuBar";
+    }
 }
