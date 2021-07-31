7ce0d91cf0961bd2e448b2bf5e78e30c90dd11d8
LK152 <wuluke0@gmail.com>
2021-07-31 18:02:43 +0800

    sassd

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let timer = setInterval(swap, 2400);
 function swap(){
-    var gif = document.getElementById("unLooped");
-    gif.src="https://2021.igem.org/wiki/images/f/f7/T--KCIS_NewTaipei--mainPageAnimationLoop.gif";
+    var gif = document.getElementById("imgBody");
+    gif.style.backgroundImage="url(https://2021.igem.org/wiki/images/f/f7/T--KCIS_NewTaipei--mainPageAnimationLoop.gif)";
     clearInterval(swap);
 }
