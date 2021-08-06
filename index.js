0d92a44df1712acc3e7497885306d436182a24d4
LK152 <wuluke0@gmail.com>
2021-08-06 16:46:34 +0800

    a

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let timer = setInterval(swap, 2400);
+
 function swap(){
     var gif = document.getElementById("imgBody");
     gif.style.backgroundImage="url(https://2021.igem.org/wiki/images/f/f7/T--KCIS_NewTaipei--mainPageAnimationLoop.gif)";
