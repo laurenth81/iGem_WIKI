702bd67428702373bd6c85c4cddffc4f2797b4b3
LK152 <wuluke0@gmail.com>
2021-08-07 14:17:24 +0800

    ss

--- template.js
+++ template.js
@@ -2,10 +2,18 @@ let progressBar = document.getElementById("progressBar");
 let totalHeight = document.body.scrollHeight - window.innerHeight;
 
 window.onscroll = function () {
-    let progressHeight = (window.pageYOffset / totalHeight) * 50;
+    let progressHeight = (window.pageYOffset / totalHeight) * 40;
     progressBar.style.height = progressHeight + "%";
 }
 
 $('#rb').bind('click', function () {
     $('html,body').animate({ scrollTop: 0 });
+});
+
+$("a[href^='#']").click(function (e) {
+    e.preventDefault();
+    var position = $($(this).attr("href")).offset().top;
+    $("body, html").animate({
+        scrollTop: position
+    });
 });
