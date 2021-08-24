fc1c270b349a97d65f783c357036c8fc2340bdef
LK152 <wuluke0@gmail.com>
2021-08-24 13:09:52 +0800

    a

--- nav.js
+++ nav.js
@@ -1,16 +1,26 @@
 const trackButton = document.getElementById("menuButton");
 const navBar = document.getElementById("navBar");
 const teamHover = document.getElementById("linkHover");
-const navLogoLink = document.getElementById("navLogoLink");
+const navId1 = document.getElementById("navId1");
+const navId2 = document.getElementById("navId2");
+const navId3 = document.getElementById("navId3");
+const navId4 = document.getElementById("navId4");
+const navId5 = document.getElementById("navId5");
 
 trackButton.addEventListener("click", menuBarSwitch);
 teamHover.addEventListener("mouseover", hideOut);
-navLogoLink.addEventListener("click", function () {
-  trackButton.classList.toggle("transit");
+navId1.addEventListener("click", logoNavSwitch);
+navId2.addEventListener("click", logoNavSwitch);
+navId3.addEventListener("click", logoNavSwitch);
+navId4.addEventListener("click", logoNavSwitch);
+navId5.addEventListener("click", logoNavSwitch);
+
+function logoNavSwitch() {
   if (navBar.className === "menuBar") {
     navBar.className += " dropDown";
+    trackButton.classList.toggle("transit");
   }
-});
+}
 
 function menuBarSwitch() {
   trackButton.classList.toggle("transit");
